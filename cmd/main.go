a5242e3be166cc1d1dc0805a674142a592ade1fd
Anton <allmightyweak@gmail.com>
2026-01-19 14:21:34 +0300

    structure cleaned

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,17 @@ package main
 import (
 	"eth-event-listener/internal/listener"
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	fmt.Println("Listener started")
